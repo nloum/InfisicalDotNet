c45c4a2cdec04d92da81410959cdb13baf1e6a77
Nathan Loum <nathan.loum@amreact.com>
2023-11-03 20:22:26 -0500

    Add an integration test

--- InfisicalDotNet.Tests/UnitTest1.cs
+++ InfisicalDotNet.Tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+
 namespace InfisicalDotNet.Tests;
 
 [TestClass]
@@ -6,5 +9,10 @@ public class UnitTest1
     [TestMethod]
     public void TestMethod1()
     {
+        var configuration = new ConfigurationBuilder()
+            .AddInfisical()
+            .Build();
+        
+        configuration["TEST_SECRET"].Should().Be("I'm used by CI tests to verify that InfisicalDotNet works");
     }
 }
